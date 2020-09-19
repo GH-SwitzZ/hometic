c0a09ee986cc74910d2094d984efd925ad448527
Supavit.S <supavit.s@kbtg.tech>
2020-09-19 14:14:39 +0700

    change to port 2009

--- server.go
+++ server.go
@@ -11,17 +11,17 @@ import (
 func main() {
 	fmt.Println("hello Hometic : I'm Gopher!!")
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"status":"active"}`))
-
-	}).Methods(http.MethodPost)
+	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
 
 	server := http.Server{
-		Addr:    "127.0.0.1:2020",
+		Addr:    "127.0.0.1:2009",
 		Handler: r,
 	}
 
 	log.Println("strat....")
 	log.Fatal(server.ListenAndServe())
 
+}
+func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"status":"active"}`))
 }
