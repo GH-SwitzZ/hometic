b597f5b21c9eb8ba4fe9343da6e44750e98ced1e
Supavit.S <supavit.s@kbtg.tech>
2020-09-19 16:19:20 +0700

    fix insert db

--- server.go
+++ server.go
@@ -9,28 +9,28 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
+
+func main() {
+	fmt.Println("hello hometic : I'm Gopher!!")
+	r := mux.NewRouter()
+	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
+	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
+	fmt.Println("addr:", addr)
+	server := http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+	log.Println("starting...")
+	log.Fatal(server.ListenAndServe())
+}
 
 type Pair struct {
 	DeviceID int64
 	UserID   int64
 }
 
-func main() {
-	fmt.Println("hello Hometic : I'm Gopher!!")
-	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
-
-	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
-	server := http.Server{
-		Addr:    addr,
-		Handler: r,
-	}
-
-	log.Println("strat....")
-	log.Fatal(server.ListenAndServe())
-
-}
 func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	var p Pair
 	err := json.NewDecoder(r.Body).Decode(&p)
@@ -41,12 +41,10 @@ func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 	fmt.Printf("pair: %#v\n", p)
-
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
