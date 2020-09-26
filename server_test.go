8959c2070795e82d12ad49c6508ab2632c2f9396
Supavit.S <supavit.s@kbtg.tech>
2020-09-26 08:58:21 +0700

    week 1

--- server_test.go
+++ server_test.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"encoding/json"
 )
+
 
 func TestCreatePairDevice(t *testing.T) {
 	payload := new(bytes.Buffer)
-	json.NewEncoder(payload).Encode(Pair{DeviceID: 1234,UserID: 4321})
+	json.NewEncoder(payload).Encode(Pair{DeviceID: 1234, UserID: 4433})
 	req := httptest.NewRequest(http.MethodPost, "/pair-device", payload)
 	rec := httptest.NewRecorder()
 
-	PairDeviceHandler(rec, req)
+	create := func(p Pair) error {
+		return nil
+	}
+
+	handler := PairDeviceHandler(CreatePairDeviceFunc(create))
+
+	handler.ServeHTTP(rec, req)
+
 	if http.StatusOK != rec.Code {
-		t.Error("expect 200 OK but get", rec.Code)
+		t.Error("expect 200 OK but got ", rec.Code)
 	}
 
 	expected := `{"status":"active"}`
 	if rec.Body.String() != expected {
-		t.Errorf("expect %q but get %q\n", expected, rec.Body.String())
+		t.Errorf("expected %q but got %q\n", expected, rec.Body.String())
 	}
 }
